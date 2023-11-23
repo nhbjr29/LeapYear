cc7f217c4e685445ed80a9da4323d9245ad8e589
Nhb <nhb_jr@yahoo.com>
2023-11-23 13:37:17 +0800

    Add statement so that user will add a number

---
+++ Conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Conversion {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give a year:");
+
+        int givenYear = Integer.valueOf(scan.nextLine());
+
+    }
+}
